6b916557c9f6e94dbbc5080cf2145cbc000ac9e0
Ovidiu Chereches <skidding@gmail.com>
2012-11-18 19:28:00 +0200

    Added toggling of sponsored posts

--- scripts/page.js
+++ scripts/page.js
@@ -3,26 +3,57 @@ var getHomepageList = function() {
   return $('ul.uiStreamHomepage');
 };
 var getSponsoredPosts = function(list) {
-  var sponsoredTags = list.find('.uiStreamAdditionalLogging');
-  return sponsoredTags.parents('li.uiStreamStory');
+  var $sponsoredTags = list.find('.uiStreamAdditionalLogging');
+  // Only return sponsored posts that have not yet been flagged
+  return $sponsoredTags.parents('li.uiStreamStory:not(.sponsoredPost)');
 };
-var announceSponsoredPosts = function(list, callback) {
+var announceSponsoredPosts = function($list, callback) {
   var children;
   window.setInterval(function() {
-    posts = getSponsoredPosts(list);
-    if (posts.length) {
-      callback(posts, posts.length);
+    $posts = getSponsoredPosts($list);
+    if ($posts.length) {
+      callback($posts, $posts.length);
     }
   }, INTERVAL_PERIOD);
+};
+var injectSponsoredMention = function($post) {
+  // Flag sponsored posts so they won't be matched by future searches
+  $post.addClass('sponsoredPost');
+  // Inject post mention bar with show button that reveals the original post
+  $post.prepend(
+    '<div class="sponsoredMention">' +
+      '<a href="#" class="toggleMention">' +
+        '<span class="show">Show ↓</span>' +
+        '<span class="hide">Hide ↑</span>' +
+      '</a> ' +
+      'sponsored post' +
+    '</div>'
+  );
+  var $sponsoredMention = $post.find('.sponsoredMention');
+  $sponsoredMention.find('.toggleMention').click(function(e) {
+    e.preventDefault();
+    $post.toggleClass('revealedSponsoredPost');
+  });
+  // Add reference to sponsored subject
+  var subjectAnchor = $post.find('.uiStreamHeadline a:last');
+  if (subjectAnchor.length) {
+    $sponsoredMention.append(' from ');
+    subjectAnchor.clone().appendTo($sponsoredMention);
+  }
+};
+$.fn.hideSponsoredPost = function() {
+  $(this).each(function(i, post) {
+    injectSponsoredMention($(this));
+  });
 };
 $(function() {
   var totalCount = 0;
   // Since there wasn't any obvious way to listen to news feed changes, we had
   // to resport to a basic interval for checking whenever new items are loaded
   // within the feed
-  announceSponsoredPosts(getHomepageList(), function(posts, count) {
+  announceSponsoredPosts(getHomepageList(), function($posts, count) {
     totalCount += count;
     console.log('Hid ' + count + ' more sponsored post. ' + totalCount + ' posts hidden so far.');
-    posts.remove();
+    $posts.hideSponsoredPost();
   });
 });
