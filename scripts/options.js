b001415e89f7511e00430abf3e358996fcb98540
Shyam Habarakada <shyam.habarakada@gmail.com>
2017-12-21 15:15:14 +0530

    Add customizations
    Add the ability to optionally hide,
    
    * likes
    * event interests (actual event attendance is not filtered out)
    * shares
    
    I’ve also added an experimental feature to filter posts based on
    keyword matching.

---
+++ scripts/options.js
@@ -0,0 +1,37 @@
+function save_options() {
+  var keywords = document.getElementById('keywords').value,
+      hideShares = document.getElementById('shares').checked,
+      hideLikes = document.getElementById('likes').checked,
+      hideEventLikes = document.getElementById('likes').checked;
+
+  chrome.storage.sync.set({
+    keywords: keywords,
+    hideShares: hideShares,
+    hideLikes: hideLikes
+  }, function() {
+    $("#status")[0].innerHTML = "Options saved.";
+    $("#status").fadeIn(400);
+    setTimeout(function() {
+      $("#status").fadeOut(1000);
+    }, 2000);
+  });
+}
+
+function restore_options() {
+  chrome.storage.sync.get({
+    keywords: null,
+    hideShares: false,
+    hideLikes: false,
+    hideEventLikes: false
+  }, function(items) {
+    if(items.keywords) {
+      $("#keywords").val(items.keywords);
+    }
+    $("#shares")[0].checked = items.hideShares;
+    $("#likes")[0].checked = items.hideLikes;
+    $("#eventlikes")[0].checked = items.hideEventLikes;
+  });
+}
+
+document.addEventListener('DOMContentLoaded', restore_options);
+document.getElementById('save').addEventListener('click', save_options);
--- scripts/page.js
+++ scripts/page.js
@@ -1,8 +1,64 @@
-var INTERVAL_PERIOD = 500; // ms
+var INTERVAL_PERIOD = 750; // ms
 
-var hideSuggestedPosts = function() {
-  $("span:contains('Suggested Post')").parents('.userContentWrapper').html("<div class='hiddenPost'>&nbsp;</div>");
-  $("span:contains('Sponsored')").parents('.userContentWrapper').html("<div class='hiddenPost'>&nbsp;</div>");
+var hideSuggestedPosts_apply = function() {
+  // only operate on the home page
+  if(window.location.pathname != "/") { return; }
+
+  function hide(selector, originalHtml) {
+    var h = encodeURIComponent(originalHtml);
+      return "<div class='hiddenPost' data-original-html='" + h + "'>&nbsp;</div>";
+  }
+
+  if(window.hideSuggestedPosts_hideLikes) {
+    $("span:contains(' likes ')").parents('.userContentWrapper').html(hide);
+    $("span:contains(' like ')").parents('.userContentWrapper').html(hide);
+  }
+  if(window.hideSuggestedPosts_hideEventLikes) {
+    $("span:contains(' is interested in ')").parents('.userContentWrapper').html(hide);
+  }
+  if(window.hideSuggestedPosts_hideShares) {
+    $("span:contains(' shared ')").parents('.userContentWrapper').html(hide);
+  }
+
+  var feedContent = $(".userContentWrapper"),
+      regexps = [];
+
+  window.hideSuggestedPosts_keywords.forEach(function(keyword) {
+    regexps.push(new RegExp(keyword.trim(), 'i'));
+  });
+
+  feedContent.each(function(i,el) {
+    var html = el.innerHTML,
+        shouldHide = false;
+
+    regexps.forEach(function(regexp) {
+      if(html.match(regexp)) {
+        shouldHide = true;
+      }
+    });
+
+    if(shouldHide) {
+      el.innerHTML = hide(i, html);
+    }
+  });
 };
 
-window.setInterval(hideSuggestedPosts, INTERVAL_PERIOD);
+chrome.storage.sync.get({
+    keywords: null,
+    hideShares: false,
+    hideLikes: false,
+    hideEventLikes: false
+  }, function(items) {
+    if(items.keywords) {
+      window.hideSuggestedPosts_keywords = items.keywords.split(/[\s,;]+/);
+    } else {
+      window.hideSuggestedPosts_keywords = [];
+    }
+    window.hideSuggestedPosts_keywords.push('Suggested Post');
+    window.hideSuggestedPosts_keywords.push('Sponsored');
+    window.hideSuggestedPosts_hideShares = items.hideShares;
+    window.hideSuggestedPosts_hideLikes = items.hideLikes;
+    window.hideSuggestedPosts_hideEventLikes = items.hideEventLikes;
+  });
+
+window.setInterval(hideSuggestedPosts_apply, INTERVAL_PERIOD);
